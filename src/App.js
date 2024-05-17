451790b0ae640dc03aa7b6d10815960b0d5b96d4
Garvit <rw3.garvit.rd@gmail.com>
2024-05-17 10:11:27 +0530

    atoms done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Adminnav from "./components/Usernav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  let role = "user";
+
+  if (role == "user") {
+    return (
+      <div>
+        <Navbar />
+        <Banner />
+        <div className="row">
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+        </div>
+      </div>
+    );
+  } else if (role == "admin") {
+    return <Adminnav />;
+  } else {
+    return <h1>404 Not found</h1>;
+  }
+};
 
 export default App;
